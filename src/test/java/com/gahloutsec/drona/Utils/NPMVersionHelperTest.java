5e4b71e0759ebebacaf9290f8289a1732ae5d310
pgahlaut994 <pgahlaut994@gmail.com>
2023-10-19 16:16:01 +0530

    Changes in test files

--- src/test/java/com/gahloutsec/drona/Utils/NPMVersionHelperTest.java
+++ src/test/java/com/gahloutsec/drona/Utils/NPMVersionHelperTest.java
@@ -33,56 +33,7 @@ public class NPMVersionHelperTest {
     public void tearDown() {
     }
 
-    /**
-     * Test of pinpointPackageVersion method, of class NPMVersionHelper.
-     */
-    @Test
-    public void testPinpointPackageVersion() {
-        System.out.println("pinpointPackageVersion");
-        String name = "browserslist";
-        String version = "^1.3";
-        String resolved = NPMVersionHelper.pinpointPackageVersion(name, version);
-        System.out.println(resolved);
-    }
-
-    /**
-     * Test of getLatestVersion method, of class NPMVersionHelper.
-     */
-    @Test
-    public void testGetLatestVersion() {
-        System.out.println("getLatestVersion");
-        
-    }
-
-    /**
-     * Test of fetchAllVersions method, of class NPMVersionHelper.
-     */
-    @Test
-    public void testFetchAllVersions() {
-        System.out.println("fetchAllVersions");
-        String name = "browserslist";
-        List<String> versions = NPMVersionHelper.fetchAllVersions(name);
-        System.out.println(versions);
-    }
-
-    /**
-     * Test of sortVersions method, of class NPMVersionHelper.
-     */
-    @Test
-    public void testSortVersions() {
-        System.out.println("sortVersions");
-        
-    }
-
-    /**
-     * Test of isMajorMinorPatchVersionFormat method, of class NPMVersionHelper.
-     */
-    @Test
-    public void testIsMajorMinorPatchVersionFormat() {
-        System.out.println("isMajorMinorPatchVersionFormat");
-        
-    }
-
+    
    
     
     
