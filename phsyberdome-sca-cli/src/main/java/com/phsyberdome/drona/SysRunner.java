56795a65dbd588dba9c79f6029b8c9926571b5fd
Pratham Gahlout <pgahlaut994@gmail.com>
2024-03-03 10:59:17 +0530

    Aggregator POM project (#24)
    * Major architectural change to modularize application and use Plugin architecture
    
    * Updated workflow to use JDK 21
    
    * Updated workflow to use JDK 21
    
    * Updated Pipeline
    
    * Updated Pipeline
    
    * Updated Pipeline
    
    * Updated Pipeline
    
    * Correct folder name & plugin artifact id
    
    * Correct aritifactID
    
    * Version bump
    
    * Added a aggregator POM project

---
+++ phsyberdome-sca-cli/src/main/java/com/phsyberdome/drona/SysRunner.java
@@ -0,0 +1,46 @@
+
+
+package com.phsyberdome.drona;
+
+import com.phsyberdome.common.utils.CommandBuilder;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author Pratham Gahlout
+ */
+public class SysRunner {
+
+    public static String run(String cmd) {
+        try {
+            String[] command = CommandBuilder.buildCommandWith(cmd);
+            Process process = Runtime.getRuntime().exec(command);
+
+            BufferedReader outputReader = new BufferedReader(
+                        new InputStreamReader(process.getInputStream())
+            );
+
+            BufferedReader errorReader = new BufferedReader(
+                        new InputStreamReader(process.getErrorStream())
+            );
+
+            String output = "";
+            String line = null;
+            while((line  = outputReader.readLine()) != null) {
+                output = output.concat(line + "\n");
+            }
+
+            outputReader.close();
+            errorReader.close();
+
+            return output;
+        } catch(IOException exception) {
+            exception.printStackTrace();
+            return null;
+        }
+    }
+   
+    
+}
